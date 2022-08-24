d164e03d98d6d5769443d46c87891bbd8909b4ea
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-24 21:31:55 +0300

    commit tree 877a782d2ddbce4f97d89f8a14c5d18cc0524307

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ Bundler.require
 
 require_relative '../lib/song'
 
-DB = { conn: SQLite3::Database.new("db/music.db") }
+DB = { conn: SQLite3::Database.new("db/music.db") }  #This is hash that contains the connection to the db 
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,41 @@
 class Song
+  attr_accessor :name ,:album,:id
 
-  attr_accessor :name, :album
-
-  def initialize(name:, album:)
-    @name = name
-    @album = album
+  def initialize(name:, album:, id:nil)
+  @id = id
+  @name = name
+  @album = album
+  end
+   
+   def self.create_table  #The less than sin is syntax for creating a multiple line string.
+    sql =  <<-SQL  
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)  #DB[:conn] is an envrionment variable in another file.Execute is a method
   end
 
-end
+#method to save created song instance to the db
+  def save
+    sql = <<-SQL
+    INSERT INTO songs (name, album) VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql,self.name,self.album) #save a song instance to the db
+   
+    #get the id of the last saved song and save it to the ruby instance for later use.
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    self  #returning the update ruby instance 
+  end
+
+#method that combines creating and saving an object instance
+  def self.create(name:,album:)
+    song = Song.new(name: name,album: album)
+    song.save   #remember that the return value for this is the creates object instance updated with its Id in the db
+  end
+
+  end
